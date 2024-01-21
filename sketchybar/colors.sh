1a5cab58eacc3b7c26de262af822fda993b63e4f
JiggyMac12 <146668871+JiggyMac12@users.noreply.github.com>
2024-01-20 22:21:31 -0500

    first commit

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -8,9 +8,9 @@ export WHITE=0xffffffff
 # export ACCENT_COLOR=0xff2cf9ed
 
 # -- Gray Scheme --
-export BAR_COLOR=0xff101314
-export ITEM_BG_COLOR=0xff353c3f
-export ACCENT_COLOR=0xffffffff
+ export BAR_COLOR=0xff101314
+ export ITEM_BG_COLOR=0xff353c3f
+ export ACCENT_COLOR=0xffffffff
 
 # -- Purple Scheme --
 # export BAR_COLOR=0xff140c42
