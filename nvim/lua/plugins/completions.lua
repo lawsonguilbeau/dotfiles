6cb1351083a951fe4a4a30200cb887c57d02142b
JiggyMac12 <146668871+JiggyMac12@users.noreply.github.com>
2024-01-31 18:17:35 -0500

    first commit

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -14,7 +14,7 @@ return {
       config = function()
         local cmp = require("cmp")
         require("luasnip.loaders.from_vscode").lazy_load()
-  
+
         cmp.setup({
           snippet = {
             expand = function(args)
