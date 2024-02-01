aa91089f855c08c9e8539f1fd5e3b4572e83bb32
JiggyMac12 <146668871+JiggyMac12@users.noreply.github.com>
2024-01-31 22:33:04 -0500

    first commit

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -9,12 +9,12 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
 
 -- line numbers
@@ -63,6 +63,8 @@ opt.iskeyword:append("-") -- consider string-string as whole word
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>")
+
+keymap.set("n", "v", "v")
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>")
