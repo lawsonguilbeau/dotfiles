4724971b44f84e134250dde653fc4b0d690f3fb8
JiggyMac12 <146668871+JiggyMac12@users.noreply.github.com>
2024-02-02 15:47:54 -0500

    first commit

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,16 +9,17 @@ return {
       vim.cmd.highlight("link NvimTreeWinSeparator NvimTreeNormal") -- put somewhere in lua config after colorscheme is set
     end,
   },
-}
 
---  seperate colorscheme
---    {
---    "catppuccin/nvim",
---    lazy = false,
---    name = "catppuccin",
---    priority = 1000,
---    config = function()
---      vim.cmd.colorscheme("catppuccin")
---      vim.cmd.highlight("link NvimTreeWinSeparator NvimTreeNormal") -- put somewhere in lua config after colorscheme is set
---    end,
---  },
+  --  seperate colorscheme
+  --    {
+  --    "catppuccin/nvim",
+  --    lazy = false,
+  --    name = "catppuccin",
+  --    priority = 1000,
+  --    config = function()
+  --      vim.cmd.colorscheme("catppuccin")
+  --      vim.cmd.highlight("link NvimTreeWinSeparator NvimTreeNormal") -- put somewhere in lua config after colorscheme is set
+  --    end,
+  --  },
+  --
+}
