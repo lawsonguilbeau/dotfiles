ee4f64c21e0edb26bc574fc6bfe181b7f8555a78
JiggyMac12 <146668871+JiggyMac12@users.noreply.github.com>
2024-02-03 11:53:27 -0500

    first commit

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,6 +6,21 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+        },
+        never_show = {},
+      },
+    },
+  },
   config = function()
     vim.keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", {})
     vim.keymap.set("n", "<leader>nf", ":Neotree buffers reveal float<CR>", {})
