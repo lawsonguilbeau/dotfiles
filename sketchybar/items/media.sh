d2e6b16855dc6e8cc76d76a1d3b2219836f8685e
JiggyMac12 <146668871+JiggyMac12@users.noreply.github.com>
2024-03-08 14:25:11 -0500

    media character change

--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -2,7 +2,7 @@
 
 sketchybar --add item media center \
            --set media label.color=$ACCENT_COLOR \
-                       label.max_chars=35 \
+                       label.max_chars=30 \
                        icon.padding_left=0 \
                        scroll_texts=on \
                        icon=􀑪             \
