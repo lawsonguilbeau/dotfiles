14e93a6e2214d52e047337f5e7f6822662418ca0
JiggyMac12 <146668871+JiggyMac12@users.noreply.github.com>
2024-04-17 15:45:28 -0400

    command changes

--- karabiner/rules.ts
+++ karabiner/rules.ts
@@ -56,6 +56,9 @@ const rules: KarabinerRules[] = [
     ],
   },
   ...createHyperSubLayers({
+    spacebar: open(
+      "raycast://extensions/stellate/mxstbr-commands/create-notion-todo",
+    ),
     // b = "B"rowse
     b: {
       g: open("https://github.com"),
