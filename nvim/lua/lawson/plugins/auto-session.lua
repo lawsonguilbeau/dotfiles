cc98f65ca4a21f69c84171809370702dc2e514eb
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-05-15 11:56:40 -0400

    added a delete session command

--- nvim/lua/lawson/plugins/auto-session.lua
+++ nvim/lua/lawson/plugins/auto-session.lua
@@ -12,5 +12,6 @@ return {
 
     keymap.set("n", "<leader>sr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
     keymap.set("n", "<leader>ss", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+    keymap.set("n", "<leader>sd", "<cmd>SessionDelete<CR>", { desc = "Delete session" }) -- deldte workspace session for current working directory
   end,
 }
