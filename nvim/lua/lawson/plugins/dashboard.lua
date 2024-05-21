f48227077d59e257e102153a7c13c66b3199e7e7
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-05-21 11:05:38 -0400

    Customized the dashboard

--- nvim/lua/lawson/plugins/dashboard.lua
+++ nvim/lua/lawson/plugins/dashboard.lua
@@ -4,7 +4,72 @@ return {
     event = "VimEnter",
     config = function()
       require("dashboard").setup({
-        -- config
+        theme = "doom", --  theme is doom and hyper default is hyper
+        config = {
+          header = {
+
+            "",
+            "",
+            "",
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+            "",
+            "",
+            "",
+            "",
+          }, --your header
+          center = {
+            {
+              icon = " ",
+              desc = "New File           ",
+              key = "e",
+              key_format = " %s", -- remove default surrounding `[]`
+              action = "ene",
+            },
+            {
+              icon = " ",
+              desc = "Toggle File Explorer           ",
+              key = "SPC ee",
+              key_format = " %s", -- remove default surrounding `[]`
+              action = "NvimTreeToggle",
+            },
+            {
+              icon = "󰱼 ",
+              desc = "Find File           ",
+              key = "SPC ff",
+              key_format = " %s", -- remove default surrounding `[]`
+              action = "Telescope find_files",
+            },
+            {
+              icon = " ",
+              desc = "Find Word           ",
+              key = "SPC fs",
+              key_format = " %s", -- remove default surrounding `[]`
+              action = "Telescope live_grep",
+            },
+            {
+              icon = "󰁯 ",
+              desc = "Restore Session For Current Directory           ",
+              key = "SPC sr",
+              key_format = " %s", -- remove default surrounding `[]`
+              action = "SessionRestore",
+            },
+            {
+              icon = " ",
+              desc = "Quit NVIM           ",
+              key = "q",
+              key_format = " %s", -- remove default surrounding `[]`
+              action = "qa",
+            },
+          },
+          footer = { "Note to self: Git gud scrub" }, --your footer
+        },
       })
 
       local keymap = vim.keymap
