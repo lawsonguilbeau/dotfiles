a68d61d2314f28ba7f9ea88bdd10da9ed5a3c0fb
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-06-11 11:33:56 -0400

    changed the style of the indentation guides

--- nvim/lua/lawson/plugins/indent-blankline.lua
+++ nvim/lua/lawson/plugins/indent-blankline.lua
@@ -1,36 +1,8 @@
--- return {
---   "lukas-reineke/indent-blankline.nvim",
---   event = { "BufReadPre", "BufNewFile" },
---   main = "ibl",
---   opts = {
---     indent = { char = "┊" },
---   },
--- }
-
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
   opts = {
-    indent = {
-      char = "│",
-      tab_char = "│",
-    },
-    scope = { show_start = false, show_end = false },
-    exclude = {
-      filetypes = {
-        "help",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "Trouble",
-        "trouble",
-        "lazy",
-        "mason",
-        "notify",
-        "toggleterm",
-        "lazyterm",
-      },
-    },
+    indent = { char = "┊" },
   },
 }
