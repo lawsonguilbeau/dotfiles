88aab3db6c7c93c888d8cbc83ca696a9cc376e60
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-06-11 11:11:48 -0400

    changed the configuration of the indentation uides

--- nvim/lua/lawson/plugins/indent-blankline.lua
+++ nvim/lua/lawson/plugins/indent-blankline.lua
@@ -1,8 +1,36 @@
+-- return {
+--   "lukas-reineke/indent-blankline.nvim",
+--   event = { "BufReadPre", "BufNewFile" },
+--   main = "ibl",
+--   opts = {
+--     indent = { char = "┊" },
+--   },
+-- }
+
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
   opts = {
-    indent = { char = "┊" },
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { show_start = false, show_end = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
   },
 }
