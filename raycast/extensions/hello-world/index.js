984e0def94c137a806761f29ef85cf2f5abd72e0
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-06-12 13:52:10 -0400

    raycast changes

---
+++ raycast/extensions/hello-world/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.tsx
+var src_exports = {};
+__export(src_exports, {
+  default: () => Command
+});
+module.exports = __toCommonJS(src_exports);
+var import_api = require("@raycast/api");
+var import_jsx_runtime = require("react/jsx-runtime");
+function Command() {
+  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_api.Detail, { markdown: "# Hello World" });
+}
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vLi4vLi4vLi4vRGV2ZWxvcGVyL1JheWNhc3QgRXh0ZW5zaW9ucy9oZWxsby13b3JsZC9zcmMvaW5kZXgudHN4Il0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgeyBEZXRhaWwgfSBmcm9tIFwiQHJheWNhc3QvYXBpXCI7XG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIENvbW1hbmQoKSB7XG4gIHJldHVybiA8RGV0YWlsIG1hcmtkb3duPVwiIyBIZWxsbyBXb3JsZFwiIC8+O1xufVxuIl0sCiAgIm1hcHBpbmdzIjogIjs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUEsaUJBQXVCO0FBR2Q7QUFETSxTQUFSLFVBQTJCO0FBQ2hDLFNBQU8sNENBQUMscUJBQU8sVUFBUyxpQkFBZ0I7QUFDMUM7IiwKICAibmFtZXMiOiBbXQp9Cg==
