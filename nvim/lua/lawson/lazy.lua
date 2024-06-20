078c5ad07d9d80557ee76a85e4fc6168bb44ca98
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-06-20 19:37:12 -0400

    lazy.nvim updates

--- nvim/lua/lawson/lazy.lua
+++ nvim/lua/lawson/lazy.lua
@@ -11,8 +11,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "lawson.plugins" }, { import = "lawson.plugins.lsp"}, }, {
- checker = {
+require("lazy").setup({ { import = "lawson.plugins" }, { import = "lawson.plugins.lsp" } }, {
+  checker = {
     enabled = true,
     notify = false,
   },
