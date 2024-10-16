b36afe165d07e4f9e0b87f19a306b679dde99483
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-10-16 12:49:34 -0400

    added a package and a script

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -15,9 +15,30 @@
       environment.systemPackages =
         [ 
           pkgs.wezterm
+          pkgs.mkalias
           pkgs.neovim
           pkgs.tmux
         ];
+
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+           name = "system-applications";
+           paths = config.environment.systemPackages;
+           pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
