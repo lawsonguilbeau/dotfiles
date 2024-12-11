3872bab84f8404bc6d594af081a5e9361a376eed
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-12-11 11:03:27 -0500

    added snacks.nvim

---
+++ nvim/lua/lawson/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = false },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
+  },
+}
