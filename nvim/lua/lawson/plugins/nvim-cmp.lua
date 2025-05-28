1e0ab3de9d35f2a4499f21d7af8bc0873b0415cd
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2025-05-27 21:23:00 -0400

    added windsurf functionaliry

--- nvim/lua/lawson/plugins/nvim-cmp.lua
+++ nvim/lua/lawson/plugins/nvim-cmp.lua
@@ -53,6 +53,7 @@ return {
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
+        { name = "codeium" },
       }),
 
       -- configure lspkind for vs-code like pictograms in completion menu
